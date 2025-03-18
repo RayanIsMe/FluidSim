86fba25a713f2f6dc50faf2dde1aa387786bee0e
RayanIsMe <87342981+RayanIsMe@users.noreply.github.com>
2025-03-18 23:30:04 +0530

    Update FS.py

--- FS.py
+++ FS.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+with st.container(border = True):
+                c1, c2 = st.columns(2)
+                with c1:
+                        st.write("")
+                        st.write("")
+                        st.title("StellarThrust")
+                        st.subheader("A Rocket Launch Simulator")
+                with c2:
+                        st.image("Logo1.jpg")
